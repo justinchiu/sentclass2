0c115c585049c09c58140bad8b98f4e125d0a9cc
Justin Chiu <justinchiu@g.harvard.edu>
2019-01-31 23:17:42 -0500

    oops

--- sentclass/models/crfnegt.py
+++ sentclass/models/crfnegt.py
@@ -93,7 +93,6 @@ class CrfNegT(CrfNeg):
         # add prior
         phi_neg = phi_neg + self.phi_b.view(1, 1, 2)
 
-        phi_y = torch.zeros(N, len(self.S)).to(self.lut.weight.device)
         psi_ybs0 = torch.diag(self.psi_ys)
         psi_ybs1 = psi_ybs0 @ self.flip
         psi_ybs = (torch.stack([psi_ybs0, psi_ybs1], 1)
@@ -117,46 +116,53 @@ class CrfNegT(CrfNeg):
         phi_b.masked_fill_(mask.unsqueeze(-1), 0)
         psi_bb.masked_fill_(mask.view(N, T, 1, 1), 0)
         #psi_bb[:,0].fill_(0)
-        psi_ybs.masked_fill_(mask.view(N, T, 1, 1, 1).expand_as(psi_ybs), 0)
+        
+        #print(phi_neg)
+        #import pdb; pdb.set_trace()
+        if self.training:
+            self._N += 1
+        if self._N > 10 and self.training:
+            # marginal density b
+            phi_yl, phi_sl, phi_negl, phi_bl, psi_bbl, psi_ybsl = self.potentials(
+                phi_s, phi_neg, phi_b, psi_bb, psi_ybs)
+            hs = torch.stack(
+                self.marginal_s(phi_yl, phi_sl, phi_negl, phi_bl, psi_bbl, psi_ybsl),
+                1
+            )
+            ps = hs.softmax(-1)
 
-        # is indexing faster (tranpose first?) or splitting?
-        # SPLIT?
-        #"""
+            phi_yl, phi_sl, phi_negl, phi_bl, psi_bbl, psi_ybsl = self.potentials(
+                phi_s, phi_neg, phi_b, psi_bb, psi_ybs)
+            hb = torch.stack(
+                self.marginal_b(phi_yl, phi_sl, phi_negl, phi_bl, psi_bbl, psi_ybsl),
+                1
+            )
+            pb = hb.softmax(-1)
+            import pdb; pdb.set_trace()
+            def stuff(i):
+                #loc = self.L.itos[l[i]]
+                asp = self.A and self.A.itos[a[i]]
+                return self.tostr(words[i]), None, asp, xp[i], yp[i], bp[i]
+                import pdb; pdb.set_trace()
+            # wordsi, loc, asp, xpi, ypi, bpi = stuff(10)       psi_ybs.masked_fill_(mask.view(N, T, 1, 1, 1).expand_as(psi_ybs), 0)
+
+        # SPLIT...MAINTAINS REFERENCE TO ORIGINAL VALUES
+        phi_y = torch.zeros(N, len(self.S)).to(self.lut.weight.device)
         phi_sl   = [x.squeeze(1) for x in phi_s.split(1, 1)]
         phi_negl = [x.squeeze(1) for x in phi_neg.split(1, 1)]
         phi_bl   = [x.squeeze(1) for x in phi_b.split(1, 1)]
         psi_bbl  = [x.squeeze(1) for x in psi_bb.split(1, 1)]
         psi_ybsl = [x.squeeze(1) for x in psi_ybs.split(1, 1)]
-        #"""
-        """
-        #[for s, n, b, bb, ybs in zip(phi_s, phi_neg, phi_b, psi_bb, psi_ybs) for y in x]
-        # Ns, Nn, Nb, Nba, Nybs 
-        ok = [y for x in zip(phi_s, phi_neg, phi_b, psi_bb, psi_ybs) for y in x]
-        import pdb; pdb.set_trace()
-
-        expr = ""
-        args = []
-        for t in range(T):
-            # phi_y : N x T x Y
-            # phi_s | x: N x T x S
-            # phi_neg | x: N x T x 2
-            # phi_b: N x T x 2
-            # psi_bb: N x T x 2 x 2
-            # psi_ybs: N x T x Y x 2 x S
-            pass
-
-        """
-        """
-        # transpose and marginalize by hand; this doesn't work since i want to mutate
-        phi_s   = phi_s.transpose(0, 1)
-        phi_neg = phi_neg.transpose(0, 1)
-        phi_b   = phi_b.transpose(0, 1)
-        psi_bb  = psi_bb.transpose(0, 1)
-        psi_ybs = psi_ybs.transpose(0, 1)
-        """
         psi_bbl[0] = psi_bbl[0][:,0].fill_(0)
+        """
+        # phi_y : N x Y
+        # phi_s | x: N x T x S
+        # phi_neg | x: N x T x 2
+        # phi_b: N x T x 2
+        # psi_bb: N x T x 2 x 2
+        # psi_ybs: N x T x Y x 2 x S
+        """
         for t in range(T):
-            #b = torch.logsumexp(phi_b[t].unsqueeze(-2) + phi_bb[t], dim=-2)
             # marginalize over b_t and s_t
             b = phi_negl[t] + phi_bl[t] + psi_bbl[t]
             # update next psi_bb
@@ -165,36 +171,74 @@ class CrfNegT(CrfNeg):
             phi_y = phi_y + torch.logsumexp(torch.logsumexp(
                 psi_ybsl[t] + b.view(N, 1, 2, 1) + phi_sl[t].view(N, 1, 1, len(self.S)),
             dim=-1), dim=-1)
-        #print(phi_neg)
         #import pdb; pdb.set_trace()
-        if self.training:
-            self._N += 1
-        if self._N > 50 and self.training:
-            # marginal density b
-            phi_yy = phi_y.clone().fill_(0)
-
-
-
-            """
-            Zt, hx, hb = ubersum(
-                "nts,ntb,ntybs,ny->nt,nts,ntb",
-                phi_s, phi_neg, psi_ybs, phi_y, batch_dims="t", modulo_total=True)
-            xp = (hx - Zt.unsqueeze(-1)).exp()
-            bp = (hb - Zt.unsqueeze(-1)).exp()
-            yp = (hy - Z.unsqueeze(-1)).exp()
-            """
-            def stuff(i):
-                #loc = self.L.itos[l[i]]
-                asp = self.A and self.A.itos[a[i]]
-                return self.tostr(words[i]), None, asp, xp[i], yp[i], bp[i]
-                import pdb; pdb.set_trace()
-            # wordsi, loc, asp, xpi, ypi, bpi = stuff(10)
         return phi_y
 
+    def potentials(self, phi_s, phi_neg, phi_b, psi_bb, psi_ybs):
+        N = psi_ybs.shape[0]
+        phi_y = torch.zeros(N, len(self.S), 1).to(self.lut.weight.device)
+        phi_sl   = [x.squeeze(1) for x in phi_s.clone().split(1, 1)]
+        phi_negl = [x.squeeze(1) for x in phi_neg.clone().split(1, 1)]
+        phi_bl   = [x.squeeze(1) for x in phi_b.clone().split(1, 1)]
+        psi_bbl  = [x.squeeze(1) for x in psi_bb.clone().split(1, 1)]
+        psi_ybsl = [x.squeeze(1) for x in psi_ybs.clone().split(1, 1)]
+        psi_bbl[0] = psi_bbl[0][:,0].fill_(0)
+        return phi_y, phi_sl, phi_negl, phi_bl, psi_bbl, psi_ybsl
 
-    def marginal_s(self, phi_y, phi_s, phi_neg, phi_b, psi_bb, psi_ybs):
-        phi_y
-        pass
+    def marginal_s(self, phi_ys, phi_sl, phi_negl, phi_bl, psi_bbl, psi_ybsl):
+        T = len(psi_ybsl)
+        N, Y, B, S = psi_ybsl[0].shape
+        py = torch.zeros(N, S).to(phi_ys)
+        # marginalize over b's
+        for t in range(T):
+            # add b_t
+            b = phi_negl[t] + phi_bl[t] + psi_bbl[t]
+            # update next psi_bb
+            if t < T-1:
+                psi_bbl[t+1] = (psi_bbl[t+1] + b.unsqueeze(-1)).logsumexp(-2)
+            psi_ybsl[t] = psi_ybsl[t] + b.view(N, 1, 2, 1)
+        # for each s_t marginalize over all other s_i
+        psi_ybsl = [psi_ybsl[t] + phi_sl[t].view(N, 1, 1, len(self.S)) for t in range(T)]
+        # needs to be in the same order as above...? oops still a mistake here
+        # need to do forward backward on b to the proper messages...
+        for t in range(T):
+            y_t = sum([
+                psi_ybsl[i].logsumexp(-1).logsumexp(-1)
+                for i in range(T) if i != t
+            ])
+            phi_sl[t] = phi_sl[t] + (psi_ybsl[t] + y_t.view(N, len(self.S), 1, 1)).logsumexp(-2).logsumexp(-2)
+        import pdb; pdb.set_trace()
+        return s
+
+    def marginal_b(self, phi_yb, phi_sl, phi_negl, phi_bl, psi_bbl, psi_ybsl):
+        T = len(psi_ybsl)
+        N, Y, B, S = psi_ybsl[0].shape
+        # marginalize over s's
+        for t in range(T):
+            phi_yb = phi_yb + (psi_ybsl[t] + phi_sl[t].view(N, 1, 1, len(self.S))).logsumexp(-1)
+        phi_b = phi_yb.logsumexp(-2)
+        phi_bl = [b + phi_b for b in phi_bl]
+        # get marginal for b's
+        # fwd alphas
+        alphas = [x for x in phi_sl]
+        psi_bbl_a = [x.clone() for x in psi_bbl]
+        for t in range(T):
+            b = phi_negl[t] + phi_bl[t] + psi_bbl_a[t]
+            if t < T-1:
+                psi_bbl_a[t+1] = (psi_bbl_a[t+1] + b.unsqueeze(-1)).logsumexp(-2)
+            alphas[t] = b
+        # bwd betas
+        betas = [x for x in phi_sl]
+        psi_bbl_b = [x.clone() for x in psi_bbl]
+        psi_bbl_b.append(psi_bbl_b[0])
+        for t in range(T-1, -1, -1):
+            b = phi_negl[t] + phi_bl[t] + (psi_bbl_b[t+1] if t < T-1 else 0)
+            if t > 0:
+                psi_bbl_b[t] = (psi_bbl_b[t] + b.unsqueeze(-2)).logsumexp(-1)
+            betas[t] = b
+        s = [a+b for a, b in zip(alphas, betas)]
+        import pdb; pdb.set_trace()
+        return s
 
     def observe(self, x, lens, l, a, y):
         raise NotImplementedError
